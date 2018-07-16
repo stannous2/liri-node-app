f31fc11cfb81ffbd20ea8305ee3b29fdfb6c6510
stannous2 <user.name>
2018-07-15 22:49:08 -0700

    updates

--- liri.js
+++ liri.js
@@ -1,1 +1,36 @@
 require("dotenv").config();
+
+let Spotify = require("node-spotify-api");
+let Twitter = require("twitter");
+
+let keys = require('./keys.js');
+
+let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+console.log(client);
+
+let arg = process.argv[2];
+let songName = process.argv[3];
+
+if (arg === "my-tweets") {
+    console.log('my tweetsssss....')
+} else if (arg === "spotify-this-song") {
+    spotify
+        .search({ type: 'track', query: songName, limit: 1 })
+        .then(function (response) {
+            // console.log(response.tracks.items[0]);
+            console.log('Song name: ' + response.tracks.items[0].name)
+            console.log('Album: ' + response.tracks.items[0].album.name);
+            console.log('Artist(s): ' + response.tracks.items[0].artists[0].name);
+            console.log('External link: ' + response.tracks.items[0].album.uri);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}else if (arg === 'movie-this') {
+
+}
+
+
+
