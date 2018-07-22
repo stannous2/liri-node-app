faee4948053b89f8ed7153c1e5e847ded95eda59
stannous2 <stannous2@yahoo.com>
2018-07-21 22:29:29 -0700

    update

--- liri.js
+++ liri.js
@@ -11,9 +11,22 @@ let client = new Twitter(keys.twitter);
 let arg = process.argv[2];
 let songName = process.argv[3];
 
+//{ screen_name: 'nodejs', count: 20 }
+
 if (arg === "my-tweets") {
-    console.log(client);
+    //console.log(client);
     console.log('my tweetsssss....')
+    // client.get('favorites/list', function(error, tweets, response)
+    // client.get('search/tweets', {screen_name: '_sn2_', count: 10}, function(error, tweets, response) {
+    //     if(error) throw error;
+    //     console.log(tweets);  // The favorites.
+    //     for (i=0;i<tweets.length;i++){
+    //         console.log("Tweet: " + tweets[i].text + " Created at: " + tweets[i].created_at);
+    //         }
+        
+        //console.log(response);  // Raw     object.
+    //   });
+    tweets();
 }
 if (arg === 'movie-this') {
     console.log('favorite movie is Saving Private Ryan...')
@@ -27,10 +40,15 @@ if (arg === "spotify-this-song"){
 }
 
 
+function tweets(){
+    client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?brocashmere=' + client.options.consumer_key + '&count=20', function(error, tweets, response) {
+        if(error) throw error;
 
-
-
-
+        for (i=0;i<tweets.length;i++){
+        console.log("Tweet: " + tweets[i].text + " Created at: " + tweets[i].created_at);
+        }
+        });
+}
 
 
 function spotifyThisSong(songName){
