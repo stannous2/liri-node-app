c6685a1d612258ebd81122944cc0efaf14313bd5
stannous2 <stannous2@yahoo.com>
2018-07-22 21:24:24 -0700

    updates

--- liri.js
+++ liri.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 
 let Spotify = require("node-spotify-api");
 let Twitter = require("twitter");
+let OMBD = require("omdb-api-client");
 
 let keys = require('./keys.js');
 
 let spotify = new Spotify(keys.spotify);
 let client = new Twitter(keys.twitter);
+let omdb = new OMBD(keys.ombd);
 
 let arg = process.argv[2];
-let songName = process.argv[3];
+let name = process.argv[3];
 
 
 if (arg === "my-tweets") {
@@ -17,27 +19,41 @@ if (arg === "my-tweets") {
     getTweets(userId, 30);
 }
 if (arg === 'movie-this') {
-    console.log('favorite movie is Saving Private Ryan...')
+    getMovie(name);
 }
 if (arg === "spotify-this-song"){
-    if(songName) {
-        spotifyThisSong(songName);
+    if(name) {
+        spotifyThisSong(name);
     } else {
         spotifyThisSong('Ace of Base');
     }
 }
 
-
 function getTweets(userId, numOfTweets){
     client.get('search/tweets', {q: userId, count: numOfTweets}, function(error, tweets, response) {
         if(error) throw error;
-        //console.log(tweets.statuses[0].text);  // The favorites.
         for (i=0;i<tweets.statuses.length;i++){
             console.log(i + ':  ' + tweets.statuses[i].text);
             }
       });
 }
 
+function getMovie(name) {
+    // list a movie using callbacks
+    omdb({t: name, apikey: '3f53756'}).list(function(error, movie) {
+        if(error) throw error;
+        console.log('Title: ' + movie.title);
+        console.log('Year: ' + movie.year);
+        console.log('Rated: ' + movie.rated);
+        console.log('Released: ' + movie.released);
+        console.log('Genres: ' + movie.genres);
+        console.log('Directors: ' + movie.directors);
+        console.log('Languages: ' + movie.languages);
+        console.log('Awards: ' + movie.awards);
+        console.log('Countries: ' + movie.countries);
+});
+    
+}
 
 function spotifyThisSong(songName){
     spotify
